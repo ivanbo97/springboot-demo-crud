77dd272e545316d6277e4fa0de1eea8bca2b39dc
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-29 10:17:08 +0300

    Adding service layer.

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeRestController.java
@@ -4,22 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-	@Autowired
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
+	
+	public EmployeeRestController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
 	
 	@GetMapping("/employees")
 	public List<Employee> getEmployees(){
 		
-		return employeeDAO.findAll();
+		return employeeService.findAll();
+	}
+	
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId){
+		
+		return employeeService.findById(employeeId);
 	}
 }
