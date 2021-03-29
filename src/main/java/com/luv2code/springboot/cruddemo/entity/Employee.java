98f616b8179e09547e55fb293f30da618fc9021e
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-29 10:00:26 +0300

    Adding annotation @Id for primary key.

--- src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -4,14 +4,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="employee")
 public class Employee {
 
-	@Column(name="id")
+	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	
 	@Column(name="first_name")
