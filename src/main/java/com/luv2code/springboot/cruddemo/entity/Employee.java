3d464ee8038169ccb9e58781d833a4c03738f775
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-29 09:33:59 +0300

    Adding Employee entity.

---
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,74 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String emailString;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(String firstName, String lastName, String emailString) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailString = emailString;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailString() {
+		return emailString;
+	}
+
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailString="
+				+ emailString + "]";
+	}
+
+}
